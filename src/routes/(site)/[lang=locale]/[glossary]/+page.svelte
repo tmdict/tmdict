bcaeaf107c764f92ec7fa0996b87fa928980a93a
slsdo <gumugum@gmail.com>
2025-04-30 16:59:19 -0700

    todo description

--- src/routes/(site)/[lang=locale]/[glossary]/+page.svelte
+++ src/routes/(site)/[lang=locale]/[glossary]/+page.svelte
@@ -5,15 +5,13 @@
   let { data } = $props();
 
   // Generate meta description
-  const cleaned = data.data.content[0].i18n[data.lang].html.replace(/(<([^>]+)>)/gi, " ").substring(1, 160);
-  const metaDescription = cleaned.substring(0, Math.min(cleaned.length, cleaned.lastIndexOf(" ")));
-  console.log(metaDescription)
+  //const cleaned = data.data.content[0].i18n[data.lang].html.replace(/(<([^>]+)>)/gi, " ").substring(1, 160);
+  //const metaDescription = cleaned.substring(0, Math.min(cleaned.length, cleaned.lastIndexOf(" ")));
 </script>
 
 <svelte:head>
   <title>{data.data.attribute.attr.name[data.lang]} | TMdict</title>
   <link rel="canonical" href="https://www.tmdict.com/{data.lang}/{data.data.attribute.ja}.{data.data.attribute.id}" />
-  <meta name="description" content="{metaDescription}">
 </svelte:head>
 
 <Entry {...data} />
